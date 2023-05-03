e02174cf7805120528ab410cf66c37714b1265e2
Aayush sahu <108727859+aayushsahu3010@users.noreply.github.com>
2023-05-03 16:24:43 +0530

    add class .js

---
+++ classs_and_obj.js
@@ -0,0 +1,36 @@
+const person={};
+ const person1 = new Object();
+
+ console.log(person);
+ console.log(person1);
+
+ person.name = "aayush ";
+  person1.name = "komi";
+
+  console.log(person);
+ console.log(person1);
+
+ console.log( typeof person);
+ console.log(typeof person1);
+
+ const arr = [1,2,3];
+  const arr1 = new Array(1,2,3) ;
+  console.log(arr);
+  console.log(arr1);
+
+
+  const mydate = new Date('october 30,2004');
+  console.log(mydate);
+  console.log(mydate.getFullYear());
+
+  console.log(Array);
+  console.log(Object);
+  console.log(Number);
+  console.log(Date);
+
+  const mynumber = new Number (100.345);
+   console.log(mynumber.toFixed(0));
+
+   const array = [1,2,3,4];//literal synatx
+    array.push(45);
+     console.log(array);
